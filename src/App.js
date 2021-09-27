74a674ba1a7b01b129be0dbff90cd52a1fe99010
Urvin shah <urvinshah5@gmail.com>
2021-09-27 15:11:31 +0530

    kanban list view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Board from "react-trello";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [columnsData, setColumnsData] = useState({
+    lanes: [],
+  });
+  const [tasksDAta, setTasksData] = useState([]);
+
+  function getCount(clgname) {
+    const counts = tasksDAta.map((task) => {
+      if (task.column_id === clgname) {
+        return { ...task, title: task.name };
+      }
+    });
+
+    return counts.filter((el) => el);
+  }
+  useEffect(() => {
+    axios
+      .get("http://demo.ciaoworks.com/practical/kanban/?getData=tasks")
+      .then((res) => {
+        const data = res.data;
+        setTasksData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://demo.ciaoworks.com/practical/kanban/?getData=columns")
+      .then((res) => {
+        const data = res.data;
+        setColumnsData({
+          lanes: data.map((el, index) => {
+            return {
+              id: index,
+              title: el.name || "New column",
+              cards: getCount(el.id),
+            };
+          }),
+        });
+      });
+  }, [tasksDAta]);
+
+  function addNewColumn() {
+    setColumnsData({
+      ...columnsData,
+      lanes: [...columnsData?.lanes, { id: "abc", title: "Urvin", cards: [] }],
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board data={columnsData} />
     </div>
   );
 }
